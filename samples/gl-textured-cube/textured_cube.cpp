3e3075212e7eb87456f3b5ad1872e8a15ecf888a
Christophe Riccio <mail@g-truc.net>
2012-09-13 18:01:39 +0200

    Test of GLM functions for perspectives

--- samples/gl-textured-cube/textured_cube.cpp
+++ samples/gl-textured-cube/textured_cube.cpp
@@ -280,14 +280,15 @@ void display()
 		float zNear = 1.0f;
 		float zFar = 500.0f;
 
+		glm::mat4 Projection = glm::perspective(45.f, (float)Window.Size.x / (float)Window.Size.y, zNear, zFar);
 		//glm::mat4 Projection = glm::perspectiveFov(45.f, (float)Window.Size.x, (float)Window.Size.y, zNear, zFar);
-
+		/*
 		glm::mat4 Projection;
 		Projection[0] = glm::vec4(xScale,      0.0f,       0.0f,                     0.0f);
 		Projection[1] = glm::vec4(0.0f,        yScale,     0.0f,                     0.0f);
 		Projection[2] = glm::vec4(0.0f,        0.0f,       zFar/(zNear-zFar),        -1.0f);
 		Projection[3] = glm::vec4(0.0f,        0.0f,       -zNear*zFar/(zFar-zNear), 0.0f);
-		
+		*/
 		glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Window.TranlationCurrent.y));
 		glm::mat4 ViewRotateX = glm::rotate(ViewTranslate, Window.RotationCurrent.y, glm::vec3(1.f, 0.f, 0.f));
 		glm::mat4 View = glm::rotate(ViewRotateX, Window.RotationCurrent.x, glm::vec3(0.f, 1.f, 0.f));
